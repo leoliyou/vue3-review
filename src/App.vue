6fa8b89c79607d299300c1416f9ded61cbb13b9e
leoliyou <liyouleoleo@gmail.com>
2023-09-05 23:31:07 +0100

    readonly

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-<h2>name:{{ name }}</h2>
-<h2>age:{{ age }}</h2>
+
 </template>
 
 <script lang="ts">
@@ -9,18 +8,10 @@ import { reactive,toRefs} from 'vue'
 
 export default {
   setup() {
-    const user = reactive({
-      name: 'xiao',
-      age:16
-    })
-    const { name,age} = toRefs(user)
-    // setInterval(() => {
-    //   name.value += "++"
-    //   age.value++
-    // },1000)
+
 
     return {
-    name,age
+    
     }
   }
 
